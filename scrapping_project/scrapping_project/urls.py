ab8608b08bbef13a8d5861b466c93e4e6185c7d1
foutfin <navinpatelkumar12@gmail.com>
2024-05-24 20:50:01 +0530

    implemented endpoints fro tag , status , blogdata , brief blogdata

--- scrapping_project/scrapping_project/urls.py
+++ scrapping_project/scrapping_project/urls.py
@@ -1,22 +1,10 @@
-"""
-URL configuration for scrapping_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from tagscrapper.views import tag_scrap_view,get_scrap_request_status_view,get_blog_view,get_blog_small_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('tag/',tag_scrap_view),
+    path('status/<int:queueid>/',get_scrap_request_status_view),
+    path('blog/<str:blogid>/',get_blog_view),
+    path('blogsmall/<str:blogid>/',get_blog_small_view)
+    
 ]
--- scrapping_project/tagscrapper/views.py
+++ scrapping_project/tagscrapper/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from scrap.mainscrapper import MainScrapper
+from scrapper.models import ScrapRequest,Blog
+from django.middleware.csrf import get_token
+import json
 
-# Create your views here.
+def tag_scrap_view(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            tag = data['tag']
+            after = "" if "after" not in data else data["after"]
+            s = MainScrapper(tag)
+            return JsonResponse(s.fetch(after=after),safe=False)
+        except:
+            return JsonResponse({ 'error': 'bad request' },status=400)
+
+    return JsonResponse({ 'error': 'unsupported method' },status=405)
+
+def get_scrap_request_status_view(request,queueid):
+    if request.method == "GET":
+        try:
+            try:
+                b = ScrapRequest.objects.get(id=queueid)
+                return JsonResponse({ "status":b.status})
+            except ScrapRequest.DoesNotExist:
+                return JsonResponse({ "status":"not"})
+        except:
+            return JsonResponse({ 'error': 'bad request' },status=400)
+    return JsonResponse({ 'error': 'unsupported method' },status=405)
+
+
+def get_blog_view(request,blogid):
+    if request.method == "GET":
+        try:
+            try:
+                b = Blog.objects.get(blog_id=blogid)
+                return JsonResponse({ 
+                    "id": b.blog_id,
+                    "title": b.title,
+                    "detail": b.detail,
+                    "blog": b.blog,
+                    "isMemberOnly": b.isMemberOnly,
+                    "tags": [ tag.tag_name for tag in b.tags.all()],
+                    "creator": {
+                        "name" : b.creator.name,
+                        "img":b.creator.img
+                    },
+
+                })
+            except Blog.DoesNotExist:
+                return JsonResponse({ "error":"not exist"})
+        except:
+            return JsonResponse({ 'error': 'bad request' },status=400)
+    return JsonResponse({ 'error': 'unsupported method' },status=405)
+
+def get_blog_small_view(request,blogid):
+    if request.method == "GET":
+        try:
+            try:
+                b = Blog.objects.get(blog_id=blogid)
+                return JsonResponse({ 
+                    "id": b.blog_id,
+                    "title": b.title,
+                    "detail": b.detail,
+                    "isMemberOnly": b.isMemberOnly,
+                    "creator": {
+                        "name" : b.creator.name,
+                        "img":b.creator.img
+                    },
+
+                })
+            except Blog.DoesNotExist:
+                return JsonResponse({ "error":"not exist"})
+        except:
+            return JsonResponse({ 'error': 'bad request' },status=400)
+    return JsonResponse({ 'error': 'unsupported method' },status=405)
