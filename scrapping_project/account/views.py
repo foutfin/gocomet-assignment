6e9c9212562b8e59d98385b5918b04935cfb63df
foutfin <navinpatelkumar12@gmail.com>
2024-05-25 13:12:18 +0530

    added auth endpoints

--- scrapping_project/account/views.py
+++ scrapping_project/account/views.py
@@ -1,3 +1,71 @@
+from django.http import JsonResponse
+import json
+from django.contrib.auth.models import User
+from django.contrib import auth
 from django.shortcuts import render
 
-# Create your views here.
+def signUp(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            username = data['username']
+            password = data['password']
+            re_password  = data['re_password']
+
+            if password == re_password:
+                if User.objects.filter(username=username).exists():
+                    return JsonResponse({ 'error': 'user already exists','code':400 })
+                else:
+                    if len(password) < 8 :
+                        return JsonResponse({ 'error': 'password is small','code':401 })
+                    else:
+                        user = User.objects.create_user(username=username, password=password)
+                        return JsonResponse({ 'success': 'user created successfully','code':200 })
+            else:
+                return JsonResponse({ 'error': 'password does not match' })
+        except:
+            return JsonResponse({ 'error': 'bad request' },status=400)
+
+        
+    return JsonResponse({ 'error': 'unsupported method' },status=405)
+
+
+def accountinfo_view(request):
+    if request.method == "GET":
+        if not request.user.is_authenticated:
+            return JsonResponse({ 'error': 'not authorised' })
+        try:
+            return JsonResponse({ 'user':request.user.username })
+        except:
+            return JsonResponse({ 'error': 'Something went wrong' })
+    return JsonResponse({ 'error': 'unsupported method' },status=405)
+
+def logIn(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            username = data['username']
+            password = data['password']
+        
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return JsonResponse({ 'success': 'User authenticated' })
+            else:
+                return JsonResponse({ 'error': 'error Authenticating' })
+
+        except:
+            return JsonResponse({ 'error': 'bad request' },status=400)
+
+    return JsonResponse({ 'error': 'unsupported method' },status=405)
+
+
+def logOut(request):
+    if request.method == "POST":
+        try:
+            auth.logout(request)
+            return JsonResponse({ 'success': 'loggeed out' })
+        except:
+            return JsonResponse({ 'error': 'Something went wrong when logging out' })
+    return JsonResponse({ 'error': 'unsupported method' },status=405)
--- scrapping_project/scrapping_project/urls.py
+++ scrapping_project/scrapping_project/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 import tagscrapper.views as view
+import account.views as accountViews
 # from tagscrapper.views import tag_scrap_view,get_scrap_request_status_view,get_blog_view,get_blog_small_view
 
 urlpatterns = [
@@ -7,6 +8,14 @@ urlpatterns = [
     path('',view.indexview),
     path('blog/<str:blogid>/',view.blogview),
     path('search/<str:tag>/', view.searchview),
+    path('login/',view.indexview),
+    path('signup/',view.indexview),
+
+
+    path('auth/signup/', accountViews.signUp),
+    path('auth/login/', accountViews.logIn),
+    path('auth/logout/', accountViews.logOut),
+    path('auth/accountinfo/', accountViews.accountinfo_view),
 
     path('tag/',view.tag_scrap_view),
     path('api/status/<int:queueid>/',view.get_scrap_request_status_view),
